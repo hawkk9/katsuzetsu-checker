3e190b1e26c64210459186e619e7ef62fb491a40
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2023-05-09 21:48:19 +0900

    diff

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import dynamic from 'next/dynamic'
+import ReactDiffViewer from 'react-diff-viewer-continued';
 
 const ReactMediaRecorder = dynamic(() => import('react-media-recorder').then((mod) => mod.ReactMediaRecorder), {
   ssr: false,
@@ -7,7 +8,6 @@ const ReactMediaRecorder = dynamic(() => import('react-media-recorder').then((mo
 
 
 const inter = Inter({ subsets: ['latin'] })
-
 
 const onStop = async (blobUrl, blob) => {
   const file = new File(
@@ -28,21 +28,29 @@ const onStop = async (blobUrl, blob) => {
   );
 };
 
+const oldText = `生麦生米生卵
+隣の客はよく柿食う客だ`;
+const newText = `生麦生米生卵
+隣の客はよく柿食う客や`;
+
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <ReactMediaRecorder
-        onStop={onStop}
-        video={false}
-        render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-          <div>
-            <button className="btn btn-green" onClick={startRecording}>Start Recording</button>
-            <button className="btn btn-red" onClick={stopRecording}>Stop Recording</button>
-          </div>
-        )}
-      />
+      <div>
+        <ReactDiffViewer oldValue={oldText} newValue={newText} splitView={true} />
+        <ReactMediaRecorder
+          onStop={onStop}
+          video={false}
+          render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+            <div>
+              <button className="btn btn-green" onClick={startRecording}>Start Recording</button>
+              <button className="btn btn-red" onClick={stopRecording}>Stop Recording</button>
+            </div>
+          )}
+        />
+      </div>
     </main>
   )
 };
