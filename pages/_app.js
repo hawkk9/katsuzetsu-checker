adcdf4a0973ada3ace51e83d604b572cf5b09697
yuichi.nishitani <hawkk9.public@gmail.com>
2023-06-25 21:43:39 +0900

    add Vercel Web Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return <ChakraProvider>
     <Component {...pageProps} />
+    <Analytics />
   </ChakraProvider>
 }
